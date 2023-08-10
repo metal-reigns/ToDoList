51a23805838da3494a8785bda8cd520250311175
metal-reigns <isniper19931221@yahoo.com>
2023-08-10 11:56:53 +0300

    Added entity in Hive

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,5 +1,13 @@
+import 'package:hive/hive.dart';
+
+part 'task_model.g.dart';
+
+@HiveType(typeId: 1)
 class Task {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   bool complete;
 
   Task(this.title, this.complete);
--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,4 +1,10 @@
+import 'package:hive/hive.dart';
+
+part 'user_model.g.dart';
+
+@HiveType(typeId: 2)
 class User {
+  @HiveField(0)
   String username;
 
   User(this.username);
